2b6ce5d4a13fe60fc9feb34d2412b009753b04ef
Devuh <david.walter.reichert@gmail.com>
2025-06-27 14:50:36 -0500

    Finish implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
 
-console.log("JS Webpack Template");
+let button = document.querySelector(".button");
+let list = document.querySelector("ul");
+
+function toggleList(list) {
+    list.classList.toggle("inactive"); 
+}
+
+button.addEventListener("click", () => toggleList(list));
+
+list.addEventListener("click", (event) => {
+    if(event.target.nodeName == "LI") {
+        button.textContent = event.target.textContent;
+        toggleList(list);
+    }
+});
